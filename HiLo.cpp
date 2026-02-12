bccca26df17e331e9c339eaebc619fca23a9eb5a
ztaglia <ztaglia@umich.edu>
2026-02-12 03:08:46 -0600

    added do-while loop to avoid calling main() (yikes) and avoided calling getGuess recursively

--- HiLo.cpp
+++ HiLo.cpp
@@ -12,34 +12,55 @@
 // when the game asks them whether they want to play again.
 
 const int MAX_GUESSES = 7;
+const int MIN_NUM = 1;
+const int MAX_NUM = 100;
 
 void ignoreLine() {
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
 int getGuess(int attempt) {
-    std::cout << "Guess #" << attempt << ": ";
-    int guess;
-    std::cin >> guess;
-    if (!std::cin) {
+    while (true) {
+        std::cout << "Guess #" << attempt << ": ";
+        int guess;
+        std::cin >> guess;
+        if (!std::cin) {
+            std::cin.clear();
+            ignoreLine();
+            std::cout << "Invalid input." << std::endl;
+            continue;
+        } else if (guess < MIN_NUM || guess > MAX_NUM) {
+            std::cout << "Invalid: Out of bounds." << std::endl;
+            continue;
+        }
         std::cin.clear();
         ignoreLine();
-        std::cout << "Invalid input." << std::endl;
-        return getGuess(attempt);
-    } 
-    if (guess < 1 || guess > 100) {
-        std::cout << "Invalid: Out of bounds." << std::endl;
-        return getGuess(attempt);
+        return guess;
     }
-    std::cin.clear();
-    ignoreLine();
-    return guess;
 }
 
-int main() {
+bool playAgain() {
+    while (true) {
+        std::cout << "Would you like to play again? (y/n)? ";
+        char playAgain;
+        std::cin >> playAgain;
+        switch(playAgain) {
+            case 'y':
+                return true;
+            case 'n':
+                return false;
+            default:
+                std::cin.clear();
+                ignoreLine();
+                std::cout << "Invalid input." << std::endl;
+        }
+    }
+}
+
+bool hiLo() {
     std::cout << "Let's play a game. I'm thinking of a number between 1 and 100. You have 7 tries to guess what it is." << std::endl;
-    int secretNumber = Random::get(1, 100);
-    assert(secretNumber >= 1 && secretNumber <= 100);
+    int secretNumber = Random::get(MIN_NUM, MAX_NUM);
+    assert(secretNumber >= MIN_NUM && secretNumber <= MAX_NUM);
     assert(MAX_GUESSES == 7);
     for (int attempt = 1; attempt <= MAX_GUESSES; attempt++) {
         int guess = getGuess(attempt);
@@ -49,19 +70,19 @@ int main() {
             std::cout << "Your guess is too low." << std::endl;
         } else {
             std::cout << "Correct! You win!" << std::endl;
+            return true;
         }
     }
     std::cout << "Sorry, you lose. The correct number was " << secretNumber << "." << std::endl;
-    while (true) {
-        std::cout << "Would you like to play again? (y/n)? ";
-        char playAgain;
-        std::cin >> playAgain;
-        if (playAgain == 'y') {
-            return main();
-        } else if (playAgain == 'n') {
-            std::cout << "Thanks for playing!" << std::endl;
-            return 0;
-        }
-    }
+    return false;
+}
+
+int main() {
+    do {
+        hiLo();
+    } while (playAgain());
+
+    std::cout << "Thanks for playing!" << std::endl;
+
     return 0;
 }
