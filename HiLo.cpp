eb697acaed443fc3360f20cbadd6d5baf03dafba
ztaglia <ztaglia@umich.edu>
2026-02-12 02:47:29 -0600

    finished and passed quiz 9, addressed all listed errors

--- HiLo.cpp
+++ HiLo.cpp
@@ -2,14 +2,37 @@
 
 #include <iostream>
 #include <cassert>
+#include <limits>
 #include "Random.h"
 
+// Update your previous solution to handle invalid guesses (e.g. ‘x’), 
+// out of bounds guesses (e.g. 0 or 101), 
+// or valid guesses that have extraneous characters (e.g. 43x). 
+// Also handle the user entering extra characters 
+// when the game asks them whether they want to play again.
+
 const int MAX_GUESSES = 7;
+
+void ignoreLine() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
 
 int getGuess(int attempt) {
     std::cout << "Guess #" << attempt << ": ";
     int guess;
     std::cin >> guess;
+    if (!std::cin) {
+        std::cin.clear();
+        ignoreLine();
+        std::cout << "Invalid input." << std::endl;
+        return getGuess(attempt);
+    } 
+    if (guess < 1 || guess > 100) {
+        std::cout << "Invalid: Out of bounds." << std::endl;
+        return getGuess(attempt);
+    }
+    std::cin.clear();
+    ignoreLine();
     return guess;
 }
 
@@ -27,6 +50,7 @@ int main() {
         } else {
             std::cout << "Correct! You win!" << std::endl;
         }
+    }
     std::cout << "Sorry, you lose. The correct number was " << secretNumber << "." << std::endl;
     while (true) {
         std::cout << "Would you like to play again? (y/n)? ";
@@ -36,6 +60,7 @@ int main() {
             return main();
         } else if (playAgain == 'n') {
             std::cout << "Thanks for playing!" << std::endl;
+            return 0;
         }
     }
     return 0;
